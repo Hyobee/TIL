80d3120c15a1a03d420e4de83ff277227dd5724a
Hyobee <choeple9168@gmail.com>
2021-07-01 15:44:32 +0900

    #10

---
+++ sparta_react/week-3/bucket_list/redux/modules/bucket.js
@@ -0,0 +1,39 @@
+// bucket.js
+
+// Actions
+const LOAD   = 'bucket/LOAD';
+const CREATE = 'bucket/CREATE';
+
+const initialState= {
+    list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
+}
+
+// Reducer
+export default function reducer(state = initialState, action = {}) {
+  switch (action.type) {
+    // do reducer stuff
+    case 'bucket/LOAD': {
+        return state;
+    } 
+    case 'bucket/CREATE': {
+        const new_bucket_list = [...state.list, action.bucket];
+        return {list: new_bucket_list}
+        break;
+    } 
+
+    default: return state;
+  }
+}
+
+// Action Creators
+export const loadBucket = (bucket) => {
+    return {
+        type: LOAD, bucket
+    }
+}
+
+export const createBucket = (bucket) => {
+    return {
+        type: CREATE, bucket
+    }
+}
---
+++ sparta_react/week-3/route-ex/src/Cat.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Cat = (props) => {
+    return (
+        <div>내 고양이 이름은 {props.match.params.cat_name}</div>
+    );
+}
+
+export default Cat;
