f2ebb689cc0c8ce71b1791e379bef7ddbc44571b
Hyobee <choeple9168@gmail.com>
2021-07-08 21:33:01 +0900

    #15

--- sparta_react/week-5/react-middleage/image-community/src/pages/Login.js
+++ sparta_react/week-5/react-middleage/image-community/src/pages/Login.js
@@ -1,43 +1,37 @@
 import React from "react";
-import { Text, Input, Grid, Button } from "../elements";
+import { Button, Grid, Input, Text } from "../elements";
+import { getCookie, setCookie } from "../shared/Cookie";
+
 
 const Login = (props) => {
-  return (
-    <React.Fragment>
-      <Grid padding="16px">
-        <Text size="32px" bold>
-          로그인
-        </Text>
+    const [id, setId] = React.useState('');
+    const [pwd, setPwd] = React.useState('');
 
-        <Grid padding="16px 0px">
-          <Input
-            label="아이디"
-            placeholder="아이디를 입력해주세요."
-            _onChange={() => {
-              console.log("아이디 입력했어!");
-            }}
-          />
-        </Grid>
+    const changeId = (e) => {
+        setId(e.target.value);
+    }
 
-        <Grid padding="16px 0px">
-          <Input
-            label="패스워드"
-            placeholder="패스워드 입력해주세요."
-            _onChange={() => {
-              console.log("패스워드 입력했어!");
-            }}
-          />
-        </Grid>
+    const changePwd = (e) => {
+        setPwd(e.target.value);
+    }
 
-        <Button
-          text="로그인하기"
-          _onClick={() => {
-            console.log("로그인 했어!");
-          }}
-        ></Button>
-      </Grid>
-    </React.Fragment>
-  );
-};
+    const login = () => {
+        setCookie("user_id", id, 3);
+        setCookie("user_pwd", pwd, 3);
+    }
+    return (
+        <React.Fragment>
+            <Grid padding={16}>
+                <Text type="heading">로그인 페이지</Text>
+            </Grid>
+            <Grid padding={16}>
+                <Input value={id} onChange={changeId} placeholder="아이디를 입력하세요."/>
+                <Input value={pwd} onChange={changePwd} type="password" placeholder="비밀번호를 입력하세요."/>
+            </Grid>
+
+            <Button __click={() => {login();}}>로그인</Button>
+        </React.Fragment>
+    )
+}
 
 export default Login;
---
+++ sparta_react/week-5/react-middleage/image-community/src/redux/modules/user.js
@@ -0,0 +1,33 @@
+import {createAction, handleAction, handleActions} from "redux-actions";
+import {produce} from 'immer';
+import { setCookie, getCookie, deleteCookie } from "../shared/Cookie";
+
+// actions
+const LOG_IN = 'LOG_IN';
+const LOG_OUT = 'LOG_OUT';
+const GET_USER = 'GET_USER';
+
+// actions creators
+const logIn = createAction(LOG_IN, (user) => ({user}));
+const logOut = createAction(LOG_OUT, (user) => ({user}));
+const getUser = createAction(GET_USER, (user) => ({user}));
+
+// initialState
+const initialState = {
+    user: null,
+    is_login: false,
+};
+
+// reducer
+export default handleActions(
+    {
+        [LOG_IN]: (state, action) => produce(state, (draft) => {
+            setCookie('is_login', 'success');
+            draft.user = action.payload.user;
+            draft.is_login = true;
+        }),
+        [LOG_OUT]: (state, action) => produce(state, (draft) => {}),
+        [GET_USER]: (state, action) => produce(state, (draft) => {}),  
+    },
+    initialState
+);
